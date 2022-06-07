cc7fc57fc6fe32d33a1582ebde65d4732ab00b7a
agouy <contact@agouy.com>
2022-06-07 09:57:41 +0000

    row and col order

--- main.R
+++ main.R
@@ -7,7 +7,7 @@ library(fastcluster)
 ctx <- tercenCtx() 
 
 do.scale <- ctx$op.value('scale', as.logical, FALSE)
-fill <- ctx$op.value('scale', as.double, 0)
+fill <- ctx$op.value('fill', as.double, 0)
 method <- ctx$op.value('method', as.character, "single")
 metric <- ctx$op.value('metric', as.character, "euclidean")
 
@@ -18,15 +18,30 @@ if(do.scale) data = t(scale(t(data)))
 rorder0 <- fastcluster::hclust.vector(data, method = method, metric = metric)$order
 corder0 <- fastcluster::hclust.vector(t(data), method = method, metric = metric)$order
 
-cresult = data.frame(
-  .ci = seq(from = 0, to = length(corder0) - 1),
-  corder = as.double(corder0)
-) %>% ctx$addNamespace()
+# cresult = data.frame(
+#   .ci = seq(from = 0, to = length(corder0) - 1),
+#   corder = as.double(corder0)
+# ) %>% ctx$addNamespace()
+# 
+# rresult = data.frame(
+#   .ri = seq(from = 0, to = length(rorder0) - 1),
+#   rorder = as.double(rorder0)
+# ) %>% ctx$addNamespace()
 
-rresult = data.frame(
-  .ri = seq(from = 0, to = length(rorder0) - 1),
-  rorder = as.double(rorder0)
-) %>% ctx$addNamespace()
+ci = seq(from = 0, to = length(corder0) - 1)
+ri = seq(from = 0, to = length(rorder0) - 1)
+
+corder = as.double(ci)
+rorder = as.double(ri)
+
+ci = ci[corder0]
+ri = ri[rorder0]
+
+cresult = data.frame(.ci = ci,
+                     corder = corder) %>% ctx$addNamespace()
+
+rresult = data.frame(.ri = ri,
+                     rorder = rorder) %>% ctx$addNamespace()
 
 list(cresult, rresult) %>% ctx$save()
 
